25ace19dfa04c07918f0b17431df537b30206200
Alexandr Avramenko <alexandr_avramenko@MacBook-Pro-EU.local>
2020-01-06 17:48:10 +0200

    add immutability-helper

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,33 +1,31 @@
-import { 
-  AUTH_IN_PROGRESS,
-  AUTH_SUCCESS,
-} from "actions/types";
+import update from "immutability-helper";
+
+import { AUTH_IN_PROGRESS, AUTH_SUCCESS } from "actions/types";
 
 const initialState = {
-  profile: {name: 'Alex'},
+  profile: { name: "Alex" },
   token: null,
-  status: 'guest', // guest // in_progress // auth
+  status: "guest" // guest // in_progress // auth
 };
 
 const reducer = (state = initialState, action) => {
   const { type } = action;
-  let newState = null;
 
-  switch(type) {
+  switch (type) {
     case AUTH_IN_PROGRESS:
-      newState = JSON.parse(JSON.stringify(state));
-      newState.status = 'in_progress';
-      return newState;
+      return update(state, {
+        status: { $set: "in_progress" },
+      });
 
     case AUTH_SUCCESS:
-      newState = JSON.parse(JSON.stringify(state));
-      newState.status = 'auth';
-      newState.profile = action.payload.profile;
-      return newState;
+      return update(state, {
+        status: { $set: "auth" },
+        profile: { $set: action.payload.profile },
+      });
 
     default:
       return state;
   }
-}
+};
 
 export default reducer;
